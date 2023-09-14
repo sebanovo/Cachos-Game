aa14115114c1c7c3acd6e7b56bca413c6901c685
Sebastian <crsebastian_el_capo_2016@hotmail.com>
2023-09-14 16:38:23 -0400

    Add project files.

---
+++ cachos-sebastian-cespedes-rodas-223047333/Dados.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace cachos_sebastian_cespedes_rodas_223047333
+{
+    partial class Dados
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 35);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(115, 110);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(142, 35);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(115, 110);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(274, 35);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(115, 110);
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(404, 35);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(115, 110);
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Location = new System.Drawing.Point(537, 35);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(115, 110);
+            this.pictureBox5.TabIndex = 4;
+            this.pictureBox5.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button1.Location = new System.Drawing.Point(212, 193);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(245, 48);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Lanzar";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(212, 292);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(245, 22);
+            this.textBox1.TabIndex = 7;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(75, 285);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 36);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Terminar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(309, 363);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(340, 17);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "cachos by: Sebastian Cespedes Rodas - 223047333";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Dados
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(661, 389);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Dados";
+            this.Text = "Cachos";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ cachos-sebastian-cespedes-rodas-223047333/Dados.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cachos_sebastian_cespedes_rodas_223047333
+{
+    public partial class Dados : Form
+    {
+        public Dados()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int dado1, dado2, dado3, dado4, dado5;
+            Random aleatorio = new Random();
+            dado1 = aleatorio.Next(1,7);
+            dado2 = aleatorio.Next(1,7);
+            dado3 = aleatorio.Next(1,7);
+            dado4 = aleatorio.Next(1,7);
+            dado5 = aleatorio.Next(1,7);
+            ponerDadoEnPicture(dado1, pictureBox1);
+            ponerDadoEnPicture(dado2, pictureBox2);
+            ponerDadoEnPicture(dado3, pictureBox3);
+            ponerDadoEnPicture(dado4, pictureBox4);
+            ponerDadoEnPicture(dado5, pictureBox5);
+
+
+            int[] dados = { dado1, dado2, dado3, dado4, dado5 };
+
+            if (EsLaGrande(dados))
+            {
+                textBox1.Text = "ES LA GRANDE!!!";
+            }
+            else if (EsPoker(dados))
+            {
+                textBox1.Text = "ES POKER!!!";
+            }
+            else if (EsFull(dados))
+            {
+                textBox1.Text = "ES FULL!!!";
+            }
+            else if (EsEscalera(dados))
+            {
+                textBox1.Text = "ES ESCALERA!!!";
+            }
+            else if (EsTrica(dados))
+            {
+                textBox1.Text = "ES TRICA!!!";
+            }
+            else if (EsPar(dados))
+            {
+                textBox1.Text = "ES PAR!!!";
+            }
+            else if (EsChanfle(dados))
+            {
+                textBox1.Text = "ES CHANFLE";
+            }
+
+
+
+
+        }
+
+        public void ponerDadoEnPicture(int dado, PictureBox picture)
+        {
+            if (dado == 1)
+            {
+                picture.Image = Properties.Resources.cara1;
+                picture.SizeMode = PictureBoxSizeMode.Zoom;
+            }
+            if (dado == 2)
+            {
+                picture.Image = Properties.Resources.cara2;
+                picture.SizeMode = PictureBoxSizeMode.Zoom;
+            }
+            if (dado == 3)
+            {
+                picture.Image = Properties.Resources.cara3;
+                picture.SizeMode = PictureBoxSizeMode.Zoom;
+            }
+            if (dado == 4)
+            {
+                picture.Image = Properties.Resources.cara4;
+                picture.SizeMode = PictureBoxSizeMode.Zoom;
+            }
+            if (dado == 5)
+            {
+                picture.Image = Properties.Resources.cara5;
+                picture.SizeMode = PictureBoxSizeMode.Zoom;
+            }
+            if (dado == 6)
+            {
+                picture.Image = Properties.Resources.cara6;
+                picture.SizeMode = PictureBoxSizeMode.Zoom;
+            }
+        }
+        private bool EsLaGrande(int[] dados)
+        {
+            // Verificar si los 5 dados son iguales
+            return dados.All(dado => dado == dados[0]);
+        }
+
+        private bool EsPoker(int[] dados)
+        {
+            // Verificar si hay 4 dados iguales y 1 diferente
+            Array.Sort(dados);
+            return (dados[0] == dados[1] && dados[1] == dados[2] && dados[2] == dados[3]) ||
+                   (dados[1] == dados[2] && dados[2] == dados[3] && dados[3] == dados[4]);
+        }
+
+        private bool EsFull(int[] dados)
+        {
+            // Verificar si hay 2 dados iguales y 3 dados iguales
+            Array.Sort(dados);
+            return (dados[0] == dados[1] && dados[2] == dados[3] && dados[3] == dados[4]) ||
+                   (dados[0] == dados[1] && dados[1] == dados[2] && dados[3] == dados[4]);
+        }
+
+        private bool EsEscalera(int[] dados)
+        {
+            // Verificar si los dados son correlativos (12345 o 23456)
+            Array.Sort(dados);
+            return (dados[0] == 1 && dados[1] == 2 && dados[2] == 3 && dados[3] == 4 && dados[4] == 5) ||
+                   (dados[0] == 2 && dados[1] == 3 && dados[2] == 4 && dados[3] == 5 && dados[4] == 6);
+        }
+
+        private bool EsTrica(int[] dados)
+        {
+            // Verificar si hay 3 dados iguales y 2 diferentes
+            Array.Sort(dados);
+            return (dados[0] == dados[1] && dados[1] == dados[2] && dados[3] != dados[4]) ||
+                   (dados[0] != dados[1] && dados[2] == dados[3] && dados[3] == dados[4]);
+        }
+
+        private bool EsPar(int[] dados)
+        {
+            // Verificar si hay 2 dados iguales y 3 diferentes
+            Array.Sort(dados);
+            return (dados[0] == dados[1] && dados[2] != dados[3] && dados[3] != dados[4]) ||
+                   (dados[0] != dados[1] && dados[1] == dados[2] && dados[3] != dados[4]);
+        }
+
+        private bool EsChanfle(int[] dados)
+        {
+            // Ninguna de las anteriores
+            return !EsLaGrande(dados) && !EsPoker(dados) && !EsFull(dados) &&
+                   !EsEscalera(dados) && !EsTrica(dados) && !EsPar(dados);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
